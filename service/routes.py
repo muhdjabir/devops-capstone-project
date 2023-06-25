32c390369324412e31df77b3e15c9882fa0d6361
muhdjabir <e0725438@u.nus.edu>
2023-06-25 09:43:08 -0400

    Corrected linting

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -86,8 +87,8 @@ def get_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f'Account with id [{account_id}] could not be found.')
-    
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -118,18 +119,16 @@ def delete_accounts(account_id):
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
-    
     if account:
         account.delete()
     else:
         abort(status.HTTP_404_NOT_FOUND, f'Account with id [{account_id}] could not be found.')
     return "", status.HTTP_204_NO_CONTENT
 
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
